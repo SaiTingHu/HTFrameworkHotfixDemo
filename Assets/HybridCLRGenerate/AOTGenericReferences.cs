17c7d709326f482e845c1872de65f5cd83b7396e
SaiTingHu <987947865@qq.com>
2025-01-02 17:53:05 +0800

    完善演示流程

--- Assets/HybridCLRGenerate/AOTGenericReferences.cs
+++ Assets/HybridCLRGenerate/AOTGenericReferences.cs
@@ -6,6 +6,7 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
 	{
 		"HTFramework.RunTime.dll",
+		"UnityEngine.CoreModule.dll",
 		"mscorlib.dll",
 	};
 	// }}
@@ -14,6 +15,19 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// }} 
 
 	// {{ AOT generic types
+	// HT.Framework.SingletonBehaviourBase.ModuleBase<object>
+	// HT.Framework.SingletonBehaviourBase<object>
+	// System.Collections.Generic.Dictionary.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection<object,object>
+	// System.Collections.Generic.Dictionary<object,object>
+	// System.Collections.Generic.EqualityComparer<object>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.IEqualityComparer<object>
+	// System.Collections.Generic.KeyValuePair<object,object>
+	// System.Collections.Generic.ObjectEqualityComparer<object>
 	// }}
 
 	public void RefMethods()
@@ -22,5 +36,6 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 		// System.Void HT.Framework.UIManager.CloseUI<object>()
 		// UnityEngine.Coroutine HT.Framework.UIManager.OpenUI<object>(object[])
 		// object[] System.Array.Empty<object>()
+		// object UnityEngine.GameObject.GetComponent<object>()
 	}
 }
